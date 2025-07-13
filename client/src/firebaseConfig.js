3c0999ac4b8abc7718a2e043c9a28a73b20cc524
Pari Patel <paripatel1515@gmail.com>
2025-07-13 12:05:26 -0400

    fix: secure information

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyss-GlZGmDE-7EKxjvsioVoKSI2eah2I",
-  authDomain: "campuslink-465401.firebaseapp.com",
-  projectId: "campuslink-465401",
-  storageBucket: "campuslink-465401.firebasestorage.app",
-  messagingSenderId: "275180162229",
-  appId: "1:275180162229:web:9fb8f46b509b83fe66c2fc",
-  measurementId: "G-KVWMY2KBXE"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
